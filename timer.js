a13c5585f69cab7b51c77deca505819b0530157f
alexfalco <afalconer02@gmail.com>
2019-12-03 16:14:36 -0500

    timer.js

---
+++ timer.js
@@ -0,0 +1,21 @@
+
+
+
+let arr = process.argv.slice(2,).map(Number).filter(num => num > 0)
+
+//filter(num => num !== NaN)
+console.log(arr)
+
+
+count = 0
+
+let beep = function(data) {
+
+  for (const index of data) {
+  count = count + 1
+  setTimeout(() => process.stdout.write('\x07'),count*3000) 
+}
+
+}
+
+beep(arr)
